6afbe63d0b21c128c45ffa43e0c8f7a9e620218e
Nkanyiso Sigwaza <Nkanyiso Sigwaza>
2021-07-19 21:48:36 +0200

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,9 +18,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index/index.html": "cec0bdbd2a222471a484666499809ae1",
-"/": "9299c451ec1f25fcd94be7a638253da0",
-"index.html": "9299c451ec1f25fcd94be7a638253da0",
+"index/index.html": "2201652e8e49c7a2bbe0ba421c7021f7",
+"/": "abd54be4000c40573c1cd8040d6dd273",
+"index.html": "abd54be4000c40573c1cd8040d6dd273",
 "main.dart.js": "86c2076339429fbf1a2120cd58d50917",
 "manifest.json": "8bf05e581adf57e5c426f2a2a2a48c9e",
 "version.json": "d3dacfe330edd28e3d2769e20c7c51b8"
